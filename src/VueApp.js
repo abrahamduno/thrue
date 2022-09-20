246a0ec1168712708b4acc8cb881a33e4fa209cc
Abraham Duno <tresduno@gmail.com>
2022-09-19 20:31:05 -0400

    reorg

--- src/VueApp.js
+++ src/VueApp.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './styles/theme.css'
-import './styles/css.css';
+import './scripts/styles/theme.css'
+import './scripts/styles/css.css';
 
 import store from './scripts/store';
 
