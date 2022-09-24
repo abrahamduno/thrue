b1336a6de8040d01725e122470396b8da284f686
Abraham Duno <tresduno@gmail.com>
2022-09-24 02:35:53 -0400

    menu fix

--- src/components/parts/main-menu.vue
+++ src/components/parts/main-menu.vue
@@ -2,8 +2,11 @@
     <div style="background: linear-gradient(180deg,#00000033, #00000000)" class="w-100 pos-fixed z-999">
 
         <div class="flex-between flex-align-start show-md_x"> 
-            <div class="flex show-md_x" >
-                <a v-show="pro_mode" class="nodeco letter-s-3 noborder n-tx tx-md n-flat clickable flex-column"
+
+
+
+            <div class="flex-start show-md_x" >
+                <a class="nodeco letter-s-3 noborder n-tx tx-md n-flat clickable flex-column"
                     href="https://gamearteesan.gitbook.io/arteesan/"  target="_blank"
                     style="border-radius: 0 30px 30px 0"
                 >
@@ -12,41 +15,57 @@
                     </span>
                 </a>
 
+                <div class="flex-row ma-2" >
+                    <button class="noborder n-tx ma-2 tx-sm  clickable flex-center border-r-15 show-md_x"
+                            :class="[!pro_mode ? 'n-conca' : 'n-inset']"
+                        @click="changeProMode"
+                        style=""
+                    >
+                            <!-- v-show="pro_mode" -->
+                        <span class="pa-2 py-4  opacity-hover-50">
+                            <i class="fas tx-sm" v-if="pro_mode" > <span> <i class="fas fa-user-tie"></i> PRO</span></i>
+                            <i class="fas tx-sm"  v-else > <span> PRO</span></i>
+                        </span>
+                    </button>
+                </div>
             </div>
-            <!-- <div v-show="!pro_mode"> </div> -->
-            <div class="flex-column">
-                <button class="noborder n-tx  tx-sm  clickable flex-center border-r-15 show-md_x"
-                        :class="[!pro_mode ? 'n-conca' : 'n-inset']"
-                    @click="changeProMode"
-                    style=""
-                >
-                    <span class="pa-2 py-4  opacity-hover-50">
-                        <i class="fas tx-sm" v-if="pro_mode" > <span> PLAY</span></i>
-                        <i class="fas tx-sm"  v-else > <span> PLAY</span></i>
 
-                        <!-- <i class="fas tx-sm" v-if="pro_mode" > <span> <i class="mr- fas tx-lg fa-bars"></i></span></i>
-                        <i class="fas tx-sm"  v-else > <span> <i class="mr- fas tx-lg fa-bars"></i></span></i> -->
-                    </span>
-                </button>
 
-                <button class="noborder n-tx mt-2 tx-sm  clickable flex-center border-r-15 show-md_x"
-                        :class="[!auto_mode ? 'n-conca' : 'n-inset']"
-                        v-if="pro_mode"
-                    @click="changeAutoMode"
-                    style=""
-                >
-                    <span class="pa-2 py-4  opacity-hover-50">
-                        <i class="fas tx-sm" v-if="pro_mode" > <small> auto</small></i>
-                        <i class="fas tx-sm"  v-else > <small> auto</small></i>
 
-                        <!-- <i class="fas tx-sm" v-if="pro_mode" > <span> <i class="mr- fas tx-lg fa-bars"></i></span></i>
-                        <i class="fas tx-sm"  v-else > <span> <i class="mr- fas tx-lg fa-bars"></i></span></i> -->
-                    </span>
-                </button>
-            </div>
             <div v-show="!pro_mode"> </div>
 
-            <div class="flex-column-r flex-md_x-row flex-align-start" v-show="pro_mode">
+
+
+
+            <div class="flex-column-r flex-md_x-row flex-align-start" >
+
+
+                <div class="flex-row ma-2" >
+                    <!-- <button class="noborder n-tx ma-2 tx-sm  clickable flex-center border-r-15 show-md_x"
+                            :class="[!pro_mode ? 'n-conca' : 'n-inset']"
+                            v-show="!pro_mode"
+                        @click="changeProMode"
+                        style=""
+                    >
+                        <span class="pa-2 py-4  opacity-hover-50">
+                            <i class="fas tx-sm" v-if="pro_mode" > <span> PRO</span></i>
+                            <i class="fas tx-sm"  v-else > <span> PRO</span></i>
+                        </span>
+                    </button> -->
+
+                    <button class="noborder n-tx ma-2 tx-sm  clickable flex-center border-r-15 show-md_x"
+                            :class="[!auto_mode ? 'n-conca' : 'n-inset']"
+                            v-show="accs_length || is_playing_test"
+                        @click="changeAutoMode"
+                        style=""
+                    >
+                        <span class="pa-2 py-3  opacity-hover-50">
+                            <i class="fas tx-sm" v-if="auto_mode" > <small> <i class="fas fa-robot"></i> auto</small></i>
+                            <i class="fas tx-sm"  v-else > <small> auto</small></i>
+                        </span>
+                    </button>
+                </div>
+
                 <button class="noborder n-tx tx-sm n-conca clickable flex-center border-r-15 mt-3 mr-3"
                     @click="changeNightMode"
                 >
@@ -67,6 +86,8 @@
             </div>
         </div>
 
+
+
         <div @click="toggleMenu"
             style=""
             class="show-xs_md flex-between flex-align-start clickable b-flat"
@@ -77,6 +98,8 @@
                 <span class="tx-ls-3 pl-1"></span>
             </div>
         </div>
+
+
         <div class="flex-between flex-align-start n-inset py-2 px-2 show-xs_md " v-show="togglers.menu"> 
             <div class="flex-column tx-xs" >
                 <div class="flex mb-4">
@@ -136,8 +159,12 @@
                 </button>
             </div>
         </div>
+
         <div class="flex-between flex-align-start n-flat py-2 show-xs_md" v-show="togglers.menu"> 
         </div>
+
+
+
     </div>
 </template>
 <script>
@@ -163,6 +190,7 @@
 	        LANG()                 { return this.$store.getters.LANG },
 	        first_acc()            { return this.$store.getters.first_acc },
 	        accs_length()          { return this.$store.getters.accs_length },
+            is_playing_test()          { return this.$store.getters.is_playing_test },
         },
         created() {
             let autoMode = JSON.parse(localStorage.getItem("autoMode"))
